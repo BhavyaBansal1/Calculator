2fd71c07079fd9d1231dee77763621d812c0e074
BhavyaBansal1 <bhavyabansal1126@gmail.com>
2023-11-30 11:11:27 +0530

    y

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
 const port = 8050;
+const path = require('path');
+// const ejs = require('ejs-mate');
 
 
-app.use((req,res)=>{
-    res.sendFile('index.html');
+app.set("view engine",'ejs')
+// app.engine('ejs','ejs-mate')
+app.set('views',path.join(__dirname,'views'));
+app.use(express.static(path.join(__dirname,'public')));
+
+
+
+// app.use((req,res)=>{
+//     res.sendFile(path.join(__dirname,'views'));
+// })
+// app.use((req,res)=>{
+//     res.sendFile(path.join(__dirname,'index.ejs'));
+
+// })
+app.get('/',(req,res)=>{
+    res.render('api');
+    // res.render('public');
 })
-
-
 
 app.listen(port,()=>{
     console.log(`live at ${port}`);
